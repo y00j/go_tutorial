a25763574bff297a82a86bcbd1517851ca70f112
Yujie Zhu <yujiezhu718@gmail.com>
2020-02-16 11:41:03 -0800

    finish struct, starting maps

--- structs/main.go
+++ structs/main.go
@@ -1,10 +1,36 @@
 package main
 
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipcode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Daly",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipcode: 94105,
+		},
+	}
 
+	jim.updateName("Jimmy")
+	jim.print()
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
 }
